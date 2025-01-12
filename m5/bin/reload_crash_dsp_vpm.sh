ddd13798d87f0304d3a95a30c7284b7bf52c2fc5
Yu Li <233liyu@gmail.com>
2025-01-12 20:40:23 +0800

    rm image

--- m5/bin/reload_crash_dsp_vpm.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-
-/etc/init.d/pns stop
-echo "recover" > /sys/kernel/debug/remoteproc/remoteproc0/recovery
-echo "recover" > /sys/kernel/debug/remoteproc/remoteproc1/recovery
-sleep 6
-/etc/init.d/pns start
